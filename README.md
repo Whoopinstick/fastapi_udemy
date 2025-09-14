7774911d557b13af80866b2ff5b1429e1df55045
Kevin Paul <k.m.paul68@gmail.com>
2025-09-14 13:55:30 -0400

    add first path operations

---
+++ README.md
@@ -0,0 +1,4 @@
+## Udemy FastAPI Course
+#### Reference Repo -
+`https://github.com/codingwithroby/fastapi-the-complete-course.git`
+
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello from fastapi-udemy!")
+from fastapi import FastAPI
+import uvicorn
 
+app = FastAPI()
+
+BOOKS = [
+    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
+    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
+    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
+    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
+    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
+]
+
+
+@app.get("/")
+async def get_root():
+    return "Hello, World"
+
+@app.get("/greeting/{user}")
+async def greet_user(user: str):
+    return {"data": f"Hello, {user}"}
+
+@app.get("/books")
+async def get_books():
+    return BOOKS
+
+@app.get("/books/{book_id}")
+async def get_book_by_id(book_id: int):
+    return BOOKS[book_id]
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
