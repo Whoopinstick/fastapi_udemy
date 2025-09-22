539c4358ce8e0b759b523235ba4028624baf2eff
Kevin Paul <k.m.paul68@gmail.com>
2025-09-21 21:56:03 -0400

    add pydantic request model example

--- main.py
+++ main.py
@@ -1,101 +1,46 @@
 from fastapi import FastAPI
 import uvicorn
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-BOOKS = [
-    {'title': 'Title One', 'author': 'Author One', 'category': 'science'},
-    {'title': 'Title Two', 'author': 'Author Two', 'category': 'science'},
-    {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
-    {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
-    {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
-]
+class Book:
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
 
 
-class Book(BaseModel):
+class BookRequest(BaseModel):
+    id: int
     title: str
     author: str
-    category: str
+    description: str | None = ""
+    rating: int = Field(gt=0, le=5)
 
 
 
-@app.get("/")
-async def get_root():
-    return "Hello, World"
+BOOKS = [Book(id=1, title="First Book", author="First Author", description="The First Book Written!", rating=5),
+        Book(id=2, title="Second Book", author="Second Author", description="The Second Book Written!", rating=4),
+        Book(id=3, title="Third Book", author="First Author", description="Another book", rating=3),
+        Book(id=4, title="Fourth Book", author="Third Author", description="A book about cool stuff", rating=5)
+         ]
 
-@app.get("/greeting/{user}")
-async def greet_user(user: str):
-    return {"data": f"Hello, {user}"}
 
 @app.get("/books")
-async def get_books(category: str = "all"):
-    if category.lower() == "all":
-        return BOOKS
-    else:
-        books_to_return = []
-        for book in BOOKS:
-            if category.lower() == book["category"].lower():
-                books_to_return.append(book)
-
-        if not books_to_return:
-            return "no results"
-        else:
-            return books_to_return
-
-@app.get("/books/{book_title}")
-async def get_book_by_title(book_title: str, category: str = "all"):
-    if category.lower() == "all":
-        for book in BOOKS:
-            if book_title.lower() == book["title"].lower():
-                return book
-            else:
-                return "no results"
-    else:
-        for book in BOOKS:
-            if book_title.lower() == book["title"].lower() and category.lower() == book["category"].lower():
-                return book
-            else:
-                return "no results"
-
-    return "no results"
+async def get_books():
+    return BOOKS
 
 
-# as a query parameter, instead of POSTing to body
 @app.post("/books")
-async def insert_book(title: str, author: str, category: str):
-    new_book = {"title": title, "author": author, "category": category}
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
     BOOKS.append(new_book)
-    return new_book
+    return {"data": new_book}
 
-# post to a request body
-@app.post("/books2")
-async def insert_book(new_book: Book):
-    book_to_insert = {"title": new_book.title, "author": new_book.author, "category": new_book.category}
-    BOOKS.append(book_to_insert)
-    return new_book
 
-@app.put("/books/{book_title}")
-async def update_book(book_title: str, updated_book: Book):
-    for book in BOOKS:
-        if book_title.lower() == book["title"].lower():
-            book["title"] = updated_book.title
-            book["author"] = updated_book.author
-            book["category"] = updated_book.category
-            return book
-        else:
-            continue
-    return "no results"
-
-@app.delete("/books/{book_title}")
-async def delete_book(book_title: str):
-    for book in BOOKS:
-        if book_title.lower() == book["title"].lower():
-            BOOKS.remove(book)
-            return "book deleted"
-
-    return "no results"
 
 
 if __name__ == "__main__":
