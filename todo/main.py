98394a477f002d019559eb0db42167f982c66f79
Kevin Paul <k.m.paul68@gmail.com>
2025-09-28 21:18:10 -0400

    add sqlite project example

---
+++ todo/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+import models
+from database import engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def get_root():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
