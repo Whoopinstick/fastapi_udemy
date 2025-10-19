8e9a89a5a2adc7fbcf36c84330b076a4d6fc94e5
Kevin Paul <k.m.paul68@gmail.com>
2025-10-19 15:02:36 -0400

    Add CRUD examples with sqlalchemy and sqlite

---
+++ todo/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: Optional[bool] = False
+
