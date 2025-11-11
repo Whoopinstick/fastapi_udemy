0cb2aacc9c7775c98aabf34689b5276130ea2632
Kevin Paul <k.m.paul68@gmail.com>
2025-11-11 15:27:00 -0500

    swap sqlite for postgres

--- examples/example_003_sqlite_connection.py
+++ examples/example_003_sqlite_connection.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg://postgres:postgres@localhost:5435/todos'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///todo/todos.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
