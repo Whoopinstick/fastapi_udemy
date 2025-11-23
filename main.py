a5392c214af8136d129b94beefc9148b4dfdea6d
Kevin Paul <k.m.paul68@gmail.com>
2025-11-22 21:53:59 -0500

    add jinja2 template example

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from todo.database import engine, Base
 from todo.routers import todo_router, auth_router, admin_router, user_router
+from fastapi.templating import Jinja2Templates
 
 Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
 app = FastAPI()
 app.include_router(todo_router)
 app.include_router(auth_router)
@@ -13,6 +16,10 @@ app.include_router(user_router)
 @app.get("/")
 def get_root():
     return {"Hello": "Todos"}
+
+@app.get("/template_test")
+def template_test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
 if __name__ == "__main__":
